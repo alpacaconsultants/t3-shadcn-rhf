c1c4ef3fab27e9bce07057d3f142843293a3e3e9
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-10 17:02:29 +0700

    working dev and prod envs

--- apps/web-app/open-next.config.ts
+++ apps/web-app/open-next.config.ts
@@ -2,5 +2,5 @@ export default {
   default: {
     // This is the default server, similar to the server-function in open-next v2
   },
-  buildCommand: "echo 'hello world'",
+  buildCommand: 'yarn build',
 };
