90bea995a19bd3144a6513c903efae2ba0164b81
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-11 14:01:40 +0700

    lint working

--- utils/config-eslint/server.js
+++ utils/config-eslint/server.js
@@ -1,3 +1,10 @@
+// Warnings are errors in production
+const OFF = 'off';
+const ERROR = 'error';
+
+// most rules should be either OFF or ERROR, but use WARNING for things that are common in development but you don't want in production
+const WARNING = process.env.NODE_ENV === 'production' ? ERROR : 'warn';
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -14,6 +21,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/no-unused-vars': WARNING,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
