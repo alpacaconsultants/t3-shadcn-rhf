86a56331343ffe6c91fdd0120248488136a1f2eb
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-11 13:53:56 +0700

    more lint stuff

--- packages/common/.eslintrc.cjs
+++ packages/common/.eslintrc.cjs
@@ -1,15 +1,15 @@
 /** @type {import("eslint").Linter.Config} */
-// module.exports = {
-//   extends: ['@strixy/eslint-config'],
-//   parser: '@typescript-eslint/parser',
-//   ignorePatterns: ['.eslintrc.cjs'],
-//   parserOptions: {
-//     project: true,
-//   },
-// };
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
-  parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
-  rules: {},
+  extends: ['@strixy/eslint-config/server.js'],
+  ignorePatterns: ['.eslintrc.cjs'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
 };
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   extends: ['plugin:@typescript-eslint/recommended'],
+//   parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
+//   rules: {},
+// };
--- packages/common/src/config/common-config.ts
+++ packages/common/src/config/common-config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 /* eslint-disable no-restricted-syntax */
 
 export const PROJECT_NAME = 'tsj';
