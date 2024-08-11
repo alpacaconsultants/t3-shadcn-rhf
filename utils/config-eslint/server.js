105f512fdee7491a1e434ab5a1fd0fff11a3a167
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-11 15:53:42 +0700

    server lint

--- utils/config-eslint/server.js
+++ utils/config-eslint/server.js
@@ -12,7 +12,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import'],
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
@@ -21,6 +21,33 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-console': [WARNING, { allow: ['warn', 'error'] }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // Node.js built-in modules
+          'external', // External modules from node_modules
+          'internal', // Internal modules (e.g., absolute imports)
+          ['parent', 'sibling', 'index'], // Relative imports
+        ],
+        pathGroups: [
+          {
+            pattern: 'src',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-useless-rename': ERROR,
+    'arrow-body-style': ERROR,
+    'prefer-template': ERROR,
     '@typescript-eslint/no-unused-vars': WARNING,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
