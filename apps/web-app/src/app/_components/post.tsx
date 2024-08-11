bfa1c2552a1bc87dc80ba82b0eda02d156f90bf6
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-11 14:20:54 +0700

    format files

--- apps/web-app/src/app/_components/post.tsx
+++ apps/web-app/src/app/_components/post.tsx
@@ -1,28 +1,26 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import { api } from "~/trpc/react";
-import styles from "../index.module.css";
+import { api } from '~/trpc/react';
+import styles from '../index.module.css';
 
 export function LatestPost() {
   const [latestPost] = api.post.getLatest.useSuspenseQuery();
 
   const utils = api.useUtils();
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const createPost = api.post.create.useMutation({
     onSuccess: async () => {
       await utils.post.invalidate();
-      setName("");
+      setName('');
     },
   });
 
   return (
     <div className={styles.showcaseContainer}>
       {latestPost ? (
-        <p className={styles.showcaseText}>
-          Your most recent post: {latestPost.name}
-        </p>
+        <p className={styles.showcaseText}>Your most recent post: {latestPost.name}</p>
       ) : (
         <p className={styles.showcaseText}>You have no posts yet.</p>
       )}
@@ -34,19 +32,9 @@ export function LatestPost() {
         }}
         className={styles.form}
       >
-        <input
-          type="text"
-          placeholder="Title"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className={styles.input}
-        />
-        <button
-          type="submit"
-          className={styles.submitButton}
-          disabled={createPost.isPending}
-        >
-          {createPost.isPending ? "Submitting..." : "Submit"}
+        <input type='text' placeholder='Title' value={name} onChange={(e) => setName(e.target.value)} className={styles.input} />
+        <button type='submit' className={styles.submitButton} disabled={createPost.isPending}>
+          {createPost.isPending ? 'Submitting...' : 'Submit'}
         </button>
       </form>
     </div>
