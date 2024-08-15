51a9232546d29cac2963ecee66f145a5a07a1d24
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-15 14:31:04 +0700

    added admin page

---
+++ apps/web-app/src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+
+export default async function Home() {
+  // const hello = await api.post.hello({ text: 'from tRPC' });
+  //
+
+  return <Box>admin page</Box>;
+}
