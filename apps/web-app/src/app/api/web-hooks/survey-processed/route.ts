d893c6029a3e2e4280b23bcd1d805982386fa7ab
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-23 16:27:30 +0700

    mock done

--- apps/web-app/src/app/api/web-hooks/survey-processed/route.ts
+++ apps/web-app/src/app/api/web-hooks/survey-processed/route.ts
@@ -1,4 +1,7 @@
+import { eq } from 'drizzle-orm';
 import { type NextRequest } from 'next/server';
+import { db } from '~/server/db';
+import { surveys } from '~/server/db/schema';
 
 export const SEARCH_PARAM_SURVERY_ID = 'surveyId';
 
@@ -8,5 +11,14 @@ export async function GET(request: NextRequest) {
   if (!surveryIdStr) return Response.json({ error: 'Survey ID is required' }, { status: 400 });
   const surveyId = +surveryIdStr;
   console.log('surveryId', surveyId);
+
+  const survey = await db.query.surveys.findFirst({
+    where: eq(surveys.id, surveyId),
+  });
+
+  if (!survey) return Response.json({ error: 'Survey not found' }, { status: 404 });
+
+  console.log('survey', survey.s3Key);
+
   return Response.json({ status: 200, message: 'OK' });
 }
--- utils/mock-brain/src/app.controller.ts
+++ utils/mock-brain/src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from 'axios';
+import * as path from 'path';
+import { createReadStream } from 'fs';
+import { stat } from 'fs/promises';
 
 interface IProcessFileInput {
   downloadUrl: string;
@@ -13,13 +16,33 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getHello(@Body() processFile: IProcessFileInput): true {
-    console.log('uploadUrl', processFile.uploadUrl);
-    setTimeout(() => {
-      console.log('Sending callback');
-      axios.get(processFile.callbackUrl);
-    }, 3000);
+    setTimeout(async () => {
+      // Create a read stream for the CSV file
+      const filePath = path.join(
+        process.env.INIT_CWD,
+        '/src/mocks/processed-file.csv',
+      );
+
+      console.log(filePath);
+      const fileStream = createReadStream(filePath);
+
+      // Get file size
+      const { size } = await stat(filePath);
+
+      console.log('uploading file...', size);
+      // Upload the file
+      await axios.put(processFile.uploadUrl, fileStream, {
+        headers: {
+          'Content-Type': 'text/csv',
+          'Content-Length': size,
+        },
+        maxBodyLength: Infinity, // This allows for larger file uploads
+        timeout: 4000,
+      });
+
+      await axios.get(processFile.callbackUrl);
+    }, 10);
     return true;
   }
 }
