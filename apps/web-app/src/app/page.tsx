7f1ea2586e8e2a24ac103a7019bba4c064a51b66
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-09-27 03:21:20 +0700

    added tailwind

--- apps/web-app/src/app/page.tsx
+++ apps/web-app/src/app/page.tsx
@@ -1,32 +1,22 @@
 import Link from 'next/link';
-import { Box } from '@mui/material';
 import { getServerAuthSession } from '~/server/auth';
 import { HydrateClient } from '~/trpc/server';
 import { CreateSurveyForm } from '~/components/modules/CreateSurveyForm';
 import { SendEmail } from '~/components/modules/SendEmail';
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: 'from tRPC' });
-  //
   const session = await getServerAuthSession();
 
   return (
     <HydrateClient>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: '100vh',
-          // backgroundImage: 'linear-gradient(to bottom, #2e026d, #15162c)',
-        }}
-      >
-        <Link href={session ? '/api/auth/signout' : '/api/auth/signin'}>{session ? 'Sign out' : 'Sign in'}</Link>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <Link href={session ? '/api/auth/signout' : '/api/auth/signin'}>
+          {session ? 'Sign out' : 'Sign in'}
+        </Link>
         {session && <Link href='/admin'>Admin Page</Link>}
         <CreateSurveyForm defaultEmail={session?.user.email ?? undefined} />
         <SendEmail />
-      </Box>
+      </div>
     </HydrateClient>
   );
 }
