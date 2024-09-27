51c58f7dbde0bbaf4b4abdb44ebef9e901e9843b
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-09-28 03:07:04 +0700

    moved sst-app

---
+++ apps/sst-app/sst-env.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    "BucketSurveyEnriched": {
+      "name": string
+      "type": "sst.aws.Bucket"
+    }
+    "BucketSurveyUploads": {
+      "name": string
+      "type": "sst.aws.Bucket"
+    }
+    "EmailAlpaca": {
+      "sender": string
+      "type": "sst.aws.Email"
+    }
+    "WebApp": {
+      "type": "sst.aws.Nextjs"
+    }
+  }
+}
+export {}
--- apps/web-app/sst-env.d.ts
+++ apps/web-app/sst-env.d.ts
@@ -1,24 +1,3 @@
-/* tslint:disable */
 /* eslint-disable */
-import "sst"
-declare module "sst" {
-  export interface Resource {
-    "BucketSurveyEnriched": {
-      "name": string
-      "type": "sst.aws.Bucket"
-    }
-    "BucketSurveyUploads": {
-      "name": string
-      "type": "sst.aws.Bucket"
-    }
-    "EmailAlpaca": {
-      "sender": string
-      "type": "sst.aws.Email"
-    }
-    "WebApp": {
-      "type": "sst.aws.Nextjs"
-      "url": string
-    }
-  }
-}
+/// <reference path="../sst-app/sst-env.d.ts" />
 export {}
